331b9f93847ee164a71c24c8694c1dfc18e0f100
Dhayv <davidhyppolitedh@gmail.com>
2024-07-14 09:31:02 -0400

    created polls models/view/urls, admin link

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. Youre at this poll index")
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %/s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Youre voting question %s. % question_id")
