f644df90eca5485a16c49f3660cba50d60f927dc
Dhayv <davidhyppolitedh@gmail.com>
2024-07-16 07:57:49 -0400

    ran isort

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,9 +2,8 @@ from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
-
+from django.views import generic
 
 from .models import Choice, Question
 
